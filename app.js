812ab56d86448031fee8dd3a42c9e1b107cc3b7f
xiekailian <460715662@qq.com>
2017-10-31 11:54:00 +0800

    加入登录注册页面，加入路由

--- app.js
+++ app.js
@@ -11,6 +11,25 @@ var session = require('express-session');
 
 var app = express();
 
+var app = express();
+app.use(session({
+    secret: 'secret',
+    cookie: {
+        maxAge: 1000 * 60 * 30
+    }
+}));
+
+app.use(function(req,res,next){
+    res.locals.user = req.session.user;   // 从session 获取 user对象
+    var err = req.session.error;   //获取错误信息
+    delete req.session.error;
+    res.locals.message = "";   // 展示的信息 message
+    if(err){
+        res.locals.message = '<div class="alert alert-danger" style="margin-bottom:20px;color:red;">'+err+'</div>';
+    }
+    next();  //中间件传递
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -19,30 +38,33 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/login', users); // 即为为路径 /login 设置路由
+app.use('/register', users); // 即为为路径 /register 设置路由
+app.use("/logout", users); // 即为为路径 /logout 设置路由
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
